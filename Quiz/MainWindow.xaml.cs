308e1a09017631cb20c93c587fcf6cbd63a801d2
MMDaemon <mettenleitermatthias+git@gmail.com>
2021-02-21 14:12:52 +0100

    Implemented loading questions from .csv

--- Quiz/MainWindow.xaml.cs
+++ Quiz/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace Quiz
 {
@@ -20,9 +12,39 @@ namespace Quiz
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Question> _questions;
+
         public MainWindow()
         {
+            _questions = new List<Question>();
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "csv files (*.csv)|*.csv";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    using (var reader = new StreamReader(openFileDialog.FileName))
+                    {
+                        reader.ReadLine();
+                        while (!reader.EndOfStream)
+                        {
+                            var line = reader.ReadLine();
+                            var values = line.Split(',');
+
+                            Question question = new Question { Text = values[0], Answers = new string[] { values[1], values[2], values[3], values[4] }, Difficulty = int.Parse(values[5]) };
+                            _questions.Add(question);
+                        }
+                    }
+                    _questions = _questions.OrderBy((question) => question.Difficulty).ToList();
+                }
+            }
         }
     }
 }
---
+++ Quiz/Question.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz
+{
+    class Question
+    {
+        public string Text { get; set; }
+        public IEnumerable<string> Answers { get; set; }
+        public int Difficulty { get; set; }
+
+    }
+}
