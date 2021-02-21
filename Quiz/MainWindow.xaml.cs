904548386f2702b199e1252c7545dcc131b9bac5
MMDaemon <mettenleitermatthias+git@gmail.com>
2021-02-21 16:41:57 +0100

    Implemented Layout and Question Logic

--- Quiz/MainWindow.xaml.cs
+++ Quiz/MainWindow.xaml.cs
@@ -1,25 +1,75 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace Quiz
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        List<Question> _questions;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _currentQuestionID;
+        private List<Question> _questions;
+
+        private Question _currentQuestion;
+        public Question CurrentQuestion
+        {
+            get => _currentQuestion;
+            set => SetField(ref _currentQuestion, value, nameof(CurrentQuestion));
+        }
+
+        private string _questionCount;
+        public string QuestionCount
+        {
+            get => _questionCount;
+            set => SetField(ref _questionCount, value, nameof(QuestionCount));
+        }
 
         public MainWindow()
         {
             _questions = new List<Question>();
             InitializeComponent();
+            this.DataContext = this;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        private void SetQuestionID(int questionID)
+        {
+            answer1.IsEnabled = true;
+            answer2.IsEnabled = true;
+            answer3.IsEnabled = true;
+            answer4.IsEnabled = true;
+
+            answer1.Background = Brushes.LightGray;
+            answer2.Background = Brushes.LightGray;
+            answer3.Background = Brushes.LightGray;
+            answer4.Background = Brushes.LightGray;
+
+            _currentQuestionID = questionID;
+            CurrentQuestion = _questions[_currentQuestionID];
+            QuestionCount = $"Frage {_currentQuestionID+1} / {_questions.Count}";
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
@@ -37,12 +87,42 @@ namespace Quiz
                             var line = reader.ReadLine();
                             var values = line.Split(',', ';');
 
-                            Question question = new Question { Text = values[0], Answers = new string[] { values[1], values[2], values[3], values[4] }, Difficulty = int.Parse(values[5]) };
+                            Question question = new Question(values[0], values[1], new string[] { values[2], values[3], values[4] }, int.Parse(values[5]));
                             _questions.Add(question);
                         }
                     }
                     _questions = _questions.OrderBy((question) => question.Difficulty).ToList();
+                    SetQuestionID(0);
                 }
+            }
+        }
+
+        private void AnswerButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Button answerButton = (System.Windows.Controls.Button)sender;
+            if (CurrentQuestion.IsCorrect(answerButton.Content.ToString())){
+                answerButton.Background = Brushes.Green;
+            }
+            else
+            {
+                answerButton.Background = Brushes.Red;
+            }
+
+            if(CurrentQuestion.IsCorrect(answer1.Content.ToString()))
+            {
+                answer1.Background = Brushes.Green;
+            }
+            if (CurrentQuestion.IsCorrect(answer2.Content.ToString()))
+            {
+                answer2.Background = Brushes.Green;
+            }
+            if (CurrentQuestion.IsCorrect(answer3.Content.ToString()))
+            {
+                answer3.Background = Brushes.Green;
+            }
+            if (CurrentQuestion.IsCorrect(answer4.Content.ToString()))
+            {
+                answer4.Background = Brushes.Green;
             }
         }
     }
--- Quiz/Question.cs
+++ Quiz/Question.cs
@@ -2,11 +2,33 @@
 
 namespace Quiz
 {
-    class Question
+    public class Question
     {
-        public string Text { get; set; }
-        public IEnumerable<string> Answers { get; set; }
-        public int Difficulty { get; set; }
+
+        private string _correctAnswer;
+        public string Text { get; }
+        public IEnumerable<string> Answers { get; }
+        public string Difficulty { get; }
+
+        public Question(string text, string correctAnswer, IEnumerable<string> otherAnswers, int difficulty)
+        {
+            Text = text;
+
+            _correctAnswer = correctAnswer;
+
+            List<string> answers = new List<string>();
+            answers.Add(correctAnswer);
+            answers.AddRange(otherAnswers);
+            answers.Shuffle();
+            Answers = answers;
+
+            Difficulty = $"Schwierigkeitsgrad {difficulty} / 5";
+        }
+
+        public bool IsCorrect(string answer)
+        {
+            return answer.Equals(_correctAnswer);
+        }
 
     }
 }
