6b585598838f8bb808298de68a3f170a602df55d
MMDaemon <mettenleitermatthias+git@gmail.com>
2021-03-10 16:43:49 +0100

    Implemented Steps to select answer.

--- Quiz/MainWindow.xaml.cs
+++ Quiz/MainWindow.xaml.cs
@@ -13,10 +13,23 @@ namespace Quiz
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private const string LOAD_QUIZ = "Quiz laden";
+        private const string CONFIRM = "Antwort bestätigen";
+        private const string NEXT = "Nächste Frage";
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private int _currentQuestionID;
         private List<Question> _questions;
+
+        private string _currentAnswer;
+
+        private string _nextButtonText = LOAD_QUIZ;
+        public string NextButtonText
+        {
+            get => _nextButtonText;
+            set => SetField(ref _nextButtonText, value, nameof(NextButtonText));
+        }
 
         private Question _currentQuestion;
         public Question CurrentQuestion
@@ -52,24 +65,7 @@ namespace Quiz
             return true;
         }
 
-        private void SetQuestionID(int questionID)
-        {
-            answer1.IsEnabled = true;
-            answer2.IsEnabled = true;
-            answer3.IsEnabled = true;
-            answer4.IsEnabled = true;
-
-            answer1.Background = Brushes.LightGray;
-            answer2.Background = Brushes.LightGray;
-            answer3.Background = Brushes.LightGray;
-            answer4.Background = Brushes.LightGray;
-
-            _currentQuestionID = questionID;
-            CurrentQuestion = _questions[_currentQuestionID];
-            QuestionCount = $"Frage {_currentQuestionID+1} / {_questions.Count}";
-        }
-
-        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        private void LoadQuiz()
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
@@ -80,62 +76,123 @@ namespace Quiz
                 if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
                 {
                     _questions.Clear();
-                    using (var reader = new StreamReader(openFileDialog.FileName))
+                    using (var reader = new StreamReader(openFileDialog.FileName, System.Text.Encoding.UTF8))
                     {
                         reader.ReadLine();
                         while (!reader.EndOfStream)
                         {
                             var line = reader.ReadLine();
-                            var values = line.Split(',', ';');
+                            var values = line.Split(';');
 
                             Question question = new Question(values[0], values[1], new string[] { values[2], values[3], values[4] }, int.Parse(values[5]));
                             _questions.Add(question);
                         }
                     }
                     _questions = _questions.OrderBy((question) => question.Difficulty).ToList();
+
                     SetQuestionID(0);
                 }
             }
         }
 
+        private void SetQuestionID(int questionID)
+        {
+            _currentAnswer = null;
+            next.IsEnabled = false;
+            NextButtonText = CONFIRM;
+
+            cover1.Visibility = Visibility.Visible;
+            cover2.Visibility = Visibility.Visible;
+            cover3.Visibility = Visibility.Visible;
+            cover4.Visibility = Visibility.Visible;
+
+            answer1.IsEnabled = true;
+            answer2.IsEnabled = true;
+            answer3.IsEnabled = true;
+            answer4.IsEnabled = true;
+
+            ResetButtonColors();
+
+            _currentQuestionID = questionID;
+            CurrentQuestion = _questions[_currentQuestionID];
+            QuestionCount = $"Frage {_currentQuestionID + 1} / {_questions.Count}";
+        }
+
+        private void ResetButtonColors()
+        {
+            answer1.Background = Brushes.LightGray;
+            answer2.Background = Brushes.LightGray;
+            answer3.Background = Brushes.LightGray;
+            answer4.Background = Brushes.LightGray;
+        }
+
+        private void EvaluateAnswer()
+        {
+            cover1.Visibility = Visibility.Hidden;
+            cover2.Visibility = Visibility.Hidden;
+            cover3.Visibility = Visibility.Hidden;
+            cover4.Visibility = Visibility.Hidden;
+
+            SetAnswerColor(answer1);
+            SetAnswerColor(answer2);
+            SetAnswerColor(answer3);
+            SetAnswerColor(answer4);
+
+            NextButtonText = NEXT;
+            if (_currentQuestionID + 1 >= _questions.Count)
+            {
+                next.IsEnabled = false;
+            }
+        }
+
+        private void SetAnswerColor(System.Windows.Controls.Button answer)
+        {
+            if (answer.Content.ToString() == _currentAnswer)
+            {
+                answer.Background = Brushes.Red;
+            }
+
+            if (CurrentQuestion.IsCorrect(answer.Content.ToString()))
+            {
+                answer.Background = Brushes.Green;
+            }
+        }
+
+        private void Cover_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Button cover = (System.Windows.Controls.Button)sender;
+            cover.Visibility = Visibility.Hidden;
+        }
+
         private void AnswerButton_Click(object sender, RoutedEventArgs e)
         {
-            System.Windows.Controls.Button answerButton = (System.Windows.Controls.Button)sender;
-            if (CurrentQuestion.IsCorrect(answerButton.Content.ToString())){
-                answerButton.Background = Brushes.Green;
-            }
-            else
+            if (NextButtonText != NEXT)
             {
-                answerButton.Background = Brushes.Red;
-            }
+                ResetButtonColors();
 
-            if(CurrentQuestion.IsCorrect(answer1.Content.ToString()))
-            {
-                answer1.Background = Brushes.Green;
-            }
-            if (CurrentQuestion.IsCorrect(answer2.Content.ToString()))
-            {
-                answer2.Background = Brushes.Green;
-            }
-            if (CurrentQuestion.IsCorrect(answer3.Content.ToString()))
-            {
-                answer3.Background = Brushes.Green;
-            }
-            if (CurrentQuestion.IsCorrect(answer4.Content.ToString()))
-            {
-                answer4.Background = Brushes.Green;
-            }
+                System.Windows.Controls.Button answerButton = (System.Windows.Controls.Button)sender;
+                answerButton.Background = Brushes.SkyBlue;
 
-            if(_currentQuestionID + 1 < _questions.Count)
-            {
+                _currentAnswer = answerButton.Content.ToString();
+
                 next.IsEnabled = true;
             }
         }
 
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
-            next.IsEnabled = false;
-            SetQuestionID(_currentQuestionID + 1);
+            switch (NextButtonText)
+            {
+                case LOAD_QUIZ:
+                    LoadQuiz();
+                    break;
+                case CONFIRM:
+                    EvaluateAnswer();
+                    break;
+                case NEXT:
+                    SetQuestionID(_currentQuestionID + 1);
+                    break;
+            }
         }
     }
 }
